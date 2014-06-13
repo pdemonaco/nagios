6c8649e0adea809800b9f7be55030e2f78c20147
Phil DeMonaco <pdemon@gmail.com>
2014-06-13 16:54:04 -0400

    Added usage function

--- aix/check_dir.pl
+++ aix/check_dir.pl
@@ -2,11 +2,11 @@
 #
 # Developed by Phil DeMonaco
 #
-# Monitor file system freespace
+# Monitor Directory Contents
 # ============================================================
 #
 # This script is to be used as a monitor command for nagios 
-# to remotely determine whether the specified fs has the 
+# to remotely determine whether the specified directory has 
 # appropriate number of files within the correct time window.
 #
 # ============================================================
@@ -38,17 +38,42 @@ my $find = "/usr/bin/find";
 # Thresholds
 my $critical, $warning;
 # Flags
-my $flag_mode;
+my $mode;
 # Settings
 my $path, $pattern;
 
 
-# Main Execution ---------------------------------------------
-## Enable bundling of single character options... risky business
+## Main Execution =============================================
+# Enable bundling of single character options... risky business
 Getopt::Long::Configure( "bundling" );
 
-## Process Parameters
-GetOptions( "mode|m"            => \$flag_mode,
-            "critical|crit|c=d" => \$critical,
-            "warning|warn|w=d"  => \$warning);
+# Process Parameters
+GetOptions( "mode|m"            => \$mode,
+            "critical|crit|c=i" => \$critical,
+            "warning|warn|w=i"  => \$warning) 
+    or usage();
 
+# All parameters are currently required
+unless( defined $mode )     { die "The mode must be specified!"; }
+unless( defined $critical ) { die "A critical threshold is required!"; }
+unless( defined $warning )  { die "A warning threshold is required!"; }
+
+
+## Usage ======================================================
+# Trivial subroutine which prints a usage message
+## ============================================================
+sub usage() {
+
+    print << "EOF";
+Usage: check_dir -m MODE -w WARNING -c CRITICAL -d DIRECTORY -p PATTERN
+
+--source    -s <path>   Path to the source data file
+
+--target    -t <path>   Path to the target output file
+
+--overwrite -o          Enable overwritting of the output file should it exist
+
+--zeros     -z          Enable the enclusion of lines with zero quantity
+
+EOF
+}
